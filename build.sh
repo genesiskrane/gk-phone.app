3b1fd961554b2e9e67208171e8894c9a2f93d2d8
Augment Chinedu <augmentchinedu@gmail.com>
2025-06-08 14:54:03 +0100

    New Build

--- build.sh
+++ build.sh
@@ -3,8 +3,7 @@ echo "Environment: $APP_NAME"
 echo "API Key: $PORT"
 
 # Clone the parent or child repository into the current directory
-cd ../
-git clone https://github.com/genesiskrane/app .
+git clone https://github.com/genesiskrane/app server
 
 npm install  # or yarn or pnpm
 
