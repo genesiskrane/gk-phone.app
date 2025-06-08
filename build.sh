08c6d43685c4ed2aefe2bf4f1932dbf055fe4646
Augment Chinedu <augmentchinedu@gmail.com>
2025-06-08 15:58:20 +0100

    ENV Used

--- build.sh
+++ build.sh
@@ -1,15 +1,9 @@
-# Now use the variables
-echo "Environment: $APP_NAME"
-echo "API Key: $PORT"
-
-
-
 mkdir server
 shopt -s extglob
 mv !(server) server/
 
 # Clone the parent or child repository into the current directory
-git clone https://github.com/genesiskrane/app 
+git clone $REPO_EXT app 
 
 mv app/* .
 
