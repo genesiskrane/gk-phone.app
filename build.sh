b3f67587711d468f667ee75ba34b6d02977800e4
Augment Chinedu <augmentchinedu@gmail.com>
2025-06-08 16:44:03 +0100

    Check Server Folder

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@ shopt -s extglob
 mv !(server) server/
 
 # Clone the parent or child repository into the current directory
-git clone $REPO_EXT app 
+git clone $REPO_EXT core 
 git clone $REPO_FRONTEND client 
 
-mv app/* .
-rm -rf app
+mv core/* .
+rm -rf core
 
 npm install 
 
@@ -16,7 +16,6 @@ npm install
 npx vite build
 
 find . -mindepth 1 ! -name 'dist' ! -path './dist/*' -exec rm -rf {} +
-ls
 
 cd ../server
 ls
