850bb98e2be2cbf6b1a9275a6ce6dbe69a99f0ed
Augment Chinedu <augmentchinedu@gmail.com>
2025-06-08 16:07:23 +0100

    New Build

--- build.sh
+++ build.sh
@@ -4,11 +4,18 @@ mv !(server) server/
 
 # Clone the parent or child repository into the current directory
 git clone $REPO_EXT app 
+git clone $REPO_FRONTEND client 
 
 mv app/* .
-
 rm -rf app
 
 npm install 
 
+cd client 
+npm run build
+
+find . -mindepth 1 ! -name 'dist' ! -path './dist/*' -exec rm -rf {} +
+ls
+
+cd ../server
 ls
