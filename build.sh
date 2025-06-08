75bb21bdf4d8f774fb5da70c51f79054e6c2dcf0
Augment Chinedu <augmentchinedu@gmail.com>
2025-06-08 15:15:03 +0100

    Added ENV Variables

--- build.sh
+++ build.sh
@@ -3,8 +3,11 @@ echo "Environment: $APP_NAME"
 echo "API Key: $PORT"
 
 # Clone the parent or child repository into the current directory
-git clone https://github.com/genesiskrane/app server
+cd ../
+git clone $REPO_EXPRESS server
 
 npm install  # or yarn or pnpm
+
+dir /s /b
 
 ls
